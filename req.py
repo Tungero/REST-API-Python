efa309d1b6be1c5171f4bec2dfe54bace9dc59da
Tungero <zadorozhnyy.roman@sspbrno.cz>
2023-06-15 00:20:56 +0200

    added req.py for testing purposes

---
+++ req.py
@@ -0,0 +1,30 @@
+import requests
+
+def get(ID=None):
+    id_ = "" if ID == None else f"/{ID}"
+    req = requests.get("http://localhost:5000/movies" + id_)
+    return req.text
+
+def post(title, description, release_year):
+    req = requests.get("http://localhost:5000/movies", json=data)    
+    return req.text
+
+def put(id, data):
+    req = requests.get(f"http://localhost:5000/movies/{id}", json=data)    
+    return req.text
+
+
+# edit the variable below to edit the body of your request
+data = {
+        'title': "some movie",
+        'description': "some description",
+        'release_year': 1992,
+    }
+
+print(get())
+
+#print(post(data))
+
+#print(put(14, data)
+
+
